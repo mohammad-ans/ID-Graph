619cca9172c44c8d1a3f7ff67fc2d89a83aa9053
mohammad-ans <ansmuhammad340@gmail.com>
2026-08-28 00:34:48 +0500

    Added testing in the shell file to run the tests before running demo

--- shell.sh
+++ shell.sh
@@ -3,9 +3,12 @@
 set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-echo "Installing demo dependencies for the scripts to run(pyyaml, psycopg2-binary)"
+echo "Installing demo dependencies for the scripts to run(pyyaml, psycopg2-binary)\n"
 pip install --break-system-packages pyyaml psycopg2-binary 2>/dev/null || pip install --quiet pyyaml psycopg2-binary
 
+echo"\n\nRunning all the tests...\n"
+python -m unittest discover -s testing -v
+
 echo "\n\n"
-echo "Running the demo with deterministic and probability modes"
+echo "Running the demo with deterministic and probability modes\n"
 python demo/run_demo.py --adaptive-detection
