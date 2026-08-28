1cc0dbd72c17212c725c8c0f537e669e77e2214e
mohammad-ans <ansmuhammad340@gmail.com>
2026-08-28 18:19:28 +0500

    Changed testing file names as test should be at beginning, it is a convention for unittest so

--- shell.sh
+++ shell.sh
@@ -6,9 +6,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 echo "Installing demo dependencies for the scripts to run(pyyaml, psycopg2-binary)\n"
 pip install --break-system-packages pyyaml psycopg2-binary 2>/dev/null || pip install --quiet pyyaml psycopg2-binary
 
-echo"\n\nRunning all the tests...\n"
+echo -e "\n\nRunning all the tests...\n"
 python -m unittest discover -s testing -v
 
-echo "\n\n"
+echo -e "\n\n"
 echo "Running the demo with deterministic and probability modes\n"
 python demo/run_demo.py --adaptive-detection
